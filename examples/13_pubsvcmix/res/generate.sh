be34ae2674ffbd04c9c813230fecf38dc3f62414
Hoa Nguyen <hoa.nguyen.vn.sg@gmail.com>
2021-08-22 19:52:29 +0700

    Adding other generate.sh shell scripts in examples (#62)
    Adding other generate.sh for the code generation.

--- examples/13_pubsvcmix/res/generate.sh
+++ examples/13_pubsvcmix/res/generate.sh
@@ -6,6 +6,8 @@ PROJ_DIR=$(dirname $RES_DIR)
 CMD=${PROJ_DIR}/../../tools/generate.sh
 
 TARGET='generated/src'
-$CMD --root $PROJ_DIR --doc res/HelloWorld.siml --target $TARGET
+$CMD --root $PROJ_DIR --doc res/LocalHelloWorld.siml --target $TARGET
+$CMD --root $PROJ_DIR --doc res/RemoteRegistry.siml --target $TARGET
+$CMD --root $PROJ_DIR --doc res/SystemShutdown.siml --target $TARGET
 
 find ${PROJ_DIR}/${TARGET} \( -name '*.cpp' -o -name '*.hpp' \) -exec dos2unix {} \;
--- examples/16_pubfsm/res/generate.sh
+++ examples/16_pubfsm/res/generate.sh
@@ -6,6 +6,7 @@ PROJ_DIR=$(dirname $RES_DIR)
 CMD=${PROJ_DIR}/../../tools/generate.sh
 
 TARGET='generated/src'
-$CMD --root $PROJ_DIR --doc res/HelloWorld.siml --target $TARGET
+$CMD --root $PROJ_DIR --doc res/PowerManager.siml --target $TARGET
+$CMD --root $PROJ_DIR --doc res/TrafficController.siml --target $TARGET
 
 find ${PROJ_DIR}/${TARGET} \( -name '*.cpp' -o -name '*.hpp' \) -exec dos2unix {} \;
