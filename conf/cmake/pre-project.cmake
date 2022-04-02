918d38d02b740737114e022c24172ed33ee65d38
Ali .N <A.Nasrolahi01@gmail.com>
2022-04-02 12:26:50 +0430

    New config file added to conf/cmake/ dir.
    Some configurations must be added before project(), therefore pre-project added

---
+++ conf/cmake/pre-project.cmake
@@ -0,0 +1,5 @@
+# To use specific compiler, set CMAKE_C_COMPILER and CMAKE_CXX_COMPILER values
+# for instance:
+# set(CMAKE_CXX_COMPILER /usr/bin/g++-4.2)
+set(CMAKE_C_COMPILER)
+set(CMAKE_CXX_COMPILER)
--- conf/cmake/user.cmake
+++ conf/cmake/user.cmake
@@ -1,7 +1,3 @@
-# To use specific compiler, set CMAKE_C_COMPILER and CMAKE_CXX_COMPILER values
-# for instance:
-# set(CMAKE_CXX_COMPILER "/usr/bin/g++-4.2")
-set(CMAKE_CXX_COMPILER)
 set(CrossCompile OFF)
 
 # ###########################################################################
