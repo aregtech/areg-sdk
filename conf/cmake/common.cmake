c330b78718c413d5123f3a7bcd6ca61fc14de1b6
Al <A.Nasrolahi01@gmail.com>
2022-06-28 08:41:43 +0430

    Adding product directory for clean-up and setting general output directories.

--- conf/cmake/common.cmake
+++ conf/cmake/common.cmake
@@ -79,3 +79,14 @@ endif()
 # Examples LD flags (-l is not necessary)
 list(APPEND exampleLDFlags areg m  stdc++ rt pthread)
 set(exampleCXXStandard "17")
+
+# Adding areg/product directory for clean-up
+set_property(
+    DIRECTORY
+    APPEND
+    PROPERTY ADDITIONAL_CLEAN_FILES ${ProjOutputDir}
+)
+# Setting output directories
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${ProjLibDir})
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${ProjBinDir})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${ProjBinDir})
