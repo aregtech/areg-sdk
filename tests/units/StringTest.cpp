a4a523bd5818d769e2c06b8dd5e2c1f99413bd9b
Harshit Shah <hshah496@gmail.com>
2023-02-15 04:24:24 +0000

    test: getSubstring

--- tests/units/StringTest.cpp
+++ tests/units/StringTest.cpp
@@ -11,5 +11,31 @@ TEST(StringTest, makeInt32)
     int16_t result_int16 = 52;
     EXPECT_EQ(String::makeInt32(test), result_int16);
     EXPECT_NE(typeid(String::makeInt32(test)), typeid(result_int16));
+}
 
+TEST(StringTest, getSubstring)
+{
+    String test("0123 456 789 0123");
+    const char *next = static_cast<const char *>(test);
+    String result = String::getSubstring(next, "0123", &next);
+    EXPECT_EQ(result, "");
+
+    result = String::getSubstring(next, "0123", &next);
+    EXPECT_EQ(result, " 456 789 ");
+
+    result = String::getSubstring(next, "0123", &next);
+    EXPECT_EQ(result, "");
+
+    next = static_cast<const char *>(test);
+    result = String::getSubstring(next, " ", &next);
+    EXPECT_EQ(result, "0123");
+
+    result = String::getSubstring(next, " ", &next);
+    EXPECT_EQ(result, "456");
+
+    result = String::getSubstring(next, " ", &next);
+    EXPECT_EQ(result, "789");
+    
+    result = String::getSubstring(next, " ", &next);
+    EXPECT_EQ(result, "0123");
 }
