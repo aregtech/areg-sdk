e16f4374f5725a3b01b9a056ebad434b634dcd28
Harshit Shah <hshah496@gmail.com>
2023-03-29 16:51:04 +0000

    test: floating points

--- tests/units/StringTest.cpp
+++ tests/units/StringTest.cpp
@@ -78,8 +78,30 @@ TEST(StringTest, TestMakeUInt64)
 {
     constexpr char *test = "52";
     constexpr uint64_t result_uint64 = 52;
-    EXPECT_EQ(String::makeInt64(test), result_uint64);
+    EXPECT_EQ(String::makeUInt64(test), result_uint64);
     
     constexpr uint16_t result_uint16 = 52;
-    EXPECT_EQ(String::makeInt64(test), result_uint16);
+    EXPECT_EQ(String::makeUInt64(test), result_uint16);
+}
+
+/**
+ * Tests makeFloat() which converts a string
+ * to a 32 bit floating point
+*/
+TEST(StringTest, TestMakeFloat)
+{
+    constexpr char *test = "3.1428571";
+    constexpr float result_float = 3.1428571;
+    EXPECT_EQ(String::makeFloat(test), result_float);
+}
+
+/**
+ * Tests makeDouble() which converts a string
+ * to a 64 bit floating point
+*/
+TEST(StringTest, TestMakeDouble)
+{
+    constexpr char *test = "3.142857142857143";
+    constexpr double result_double = 3.142857142857143;
+    EXPECT_EQ(String::makeDouble(test), result_double);
 }
