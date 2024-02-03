bf0754cdce6cc19b199711e7264ac16b542acfcc
Artak Avetyan <artak@aregtech.com>
2024-02-03 22:46:05 +0100

    Updated scripts to copy service start files for Windows and Linux. Fixed build warning.

--- framework/mcrouter/resources/mcrouter_uninstall.bat
+++ framework/mcrouter/resources/mcrouter_uninstall.bat
@@ -1,7 +1,8 @@
-rem : run this file under Windows with Administrator rights
-rem : to uninstall the AREG Multicast Router Service on local machine
-
 @echo off
+
+rem : To uninstall the AREG Multicast Router Service on local machine
+rem : run this file under Windows with Administrator rights
+
 mcrouter.exe --uninstall
 
 exit
