62b4c24ee8e5351a3045f800abd11b60849237fd
Aregtech <49253642+aregtech@users.noreply.github.com>
2024-10-11 13:50:03 +0200

    374 fix demo project build (#375)
    * Fixed the problems to compile AREG Framework with fetching in CMake.
    * Fixed working with vcpkg configuration
    * Restructuring, slight refactoring and fixing paths in the scripts.

--- framework/logger/resources/logger_post_build.bat
+++ framework/logger/resources/logger_post_build.bat
@@ -1,23 +1,19 @@
 @echo on
 SETLOCAL
 
-:: initialize passed variables. add if needed
-
-:: full path of project resource folder
-set PROJECT_RESOUR_DIR=%1
-
-:: full path of target folder -- compiled executable folder
+:: full path of resource folder
+set RESOURCE_DIR=%1
+:: full path of target folder -- output folder
 set TARGET_DIR=%2
 
+:: set the 'system32' in the PATH to call 'copy' command
 set OLDPATH=%PATH%
 set PATH=%SystemRoot%\System32;%PATH%
 
-:: copy dependencies
+:: create 'config' directory if does not exist and copy file
+IF NOT EXIST "%TARGET_DIR%config" mkdir "%TARGET_DIR%config"
+copy /D /y "%RESOURCE_DIR%logger.service.install.bat"   "%TARGET_DIR%config\"
+copy /D /y "%RESOURCE_DIR%logger.service.uninstall.bat" "%TARGET_DIR%config\"
 
-copy /D /y %PROJECT_RESOUR_DIR%\logger.service.install.bat   %TARGET_DIR%\logger.service.install.bat
-copy /D /y %PROJECT_RESOUR_DIR%\logger.service.uninstall.bat %TARGET_DIR%\logger.service.uninstall.bat
-
+:: restore PATH
 set PATH=%OLDPATH%
-
-set TARGET_DIR=
-set PROJECT_RESOUR_DIR=
