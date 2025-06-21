cb2b685d74f9f805ceb829c363dc54731619bcf8
Aregtech <artak@aregtech.com>
2025-06-22 00:31:32 +0200

    Fixed bug returning value by reference.

--- framework/areglogger/client/private/LogObserverBase.cpp
+++ framework/areglogger/client/private/LogObserverBase.cpp
@@ -119,7 +119,7 @@ bool LogObserverBase::getConfigLoggerEnabled(void) const
     return logObserverConfigLoggerEnabled();
 }
 
-const std::string & LogObserverBase::getConfigLoggerAddress(void) const
+const std::string LogObserverBase::getConfigLoggerAddress(void) const
 {
     return LoggerClient::getInstance().getConfigLoggerAddress().getData();
 }
