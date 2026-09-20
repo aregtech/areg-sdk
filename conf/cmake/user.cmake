f57f212242f901b3490ca1ab81a7938089d780d6
Aregtech <artak@aregtech.com>
2026-09-20 18:05:51 +0200

    Revert "Temporary set default build to Debug."
    This reverts commit 8520965a6e7345c6a4895370509f1c9af61171b2.

--- conf/cmake/user.cmake
+++ conf/cmake/user.cmake
@@ -226,7 +226,7 @@ endif()
 
 # Set build configuration. Set "Debug" for debug build, and "Release" for release build.
 if (NOT DEFINED CMAKE_BUILD_TYPE OR "${CMAKE_BUILD_TYPE}" STREQUAL "")
-    set(CMAKE_BUILD_TYPE "Debug")
+    set(CMAKE_BUILD_TYPE "Release")
 endif()
 
 # Set the Areg binary library type to compile. Set "shared" if not "static"
